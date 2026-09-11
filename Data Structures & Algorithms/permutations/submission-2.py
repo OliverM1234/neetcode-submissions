79faf678ea93ae974e9ea947ad9999f1f0f44736
Oliver Mariyanayagam <oliver122007@gmail.com>
2026-09-11 14:51:08 +0100

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        taken = [False] * len(nums)
+        res = []
+
+        def genPerms(taken, perm):
+            
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+
+            for i in range(len(taken)):
+                
+                if not taken[i]:
+
+                    perm.append(nums[i])
+                    taken[i] = True
+                    genPerms(taken, perm)
+                    perm.pop()
+                    taken[i] = False
+
+        genPerms(taken, [])
+        return res
